76dc5aa6158b8a49da3a7ab84e96cf2fa2e7146d
SjdTl <terlouwsjoerd@outlook.com>
2025-10-22 15:04:56 +0200

    added documentation to interface

--- interface.py
+++ interface.py
@@ -7,20 +7,48 @@ import os as os
 import numpy as np
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-def combine_dataframes(crop_type = "potatoes"):
+def interface(crop_type : str = "potatoes", 
+              area : float = 10000,
+              ):
+    """
+    Description
+    -----------
+    Interface between the energy output, usage, agricultural and economical module.
+    For the interconnections, see the figure.
+
+    Parameters
+    ----------
+    crop_type : string
+        Crop used
+    area : float 
+        Area covered by the solar panels in m^2
+
+    Returns
+    -------
+    monthly_df : pd.Dataframe
+        Dataframe with all the monthly parameters:
+
+    single_df : pd.Dataframe
+        Dataframe with all the single-time parameters (that hold for the entire year or lifetime)
+
+    Notes
+    -----
+    See overview.svg
+    """
+
     df_energyOut = energy_output()
     df_energyUse = energy_usage()
     df_energyOut["Energy export [kWh]"] = df_energyOut["Energy output [kWh]"] - df_energyUse["Energy usage [kWh]"]
 
     df_agricultural = agricultural(crop_type = crop_type, irradiation_crop= np.array(df_energyOut["Irradiation crops [kWh/m^2]"]))
-    df_economicsSingle = economics(area=10000,
+    df_economicsSingle = economics(area=area,
                                    coverage =0.5,
                                    rows =10,
                                    length_rows =1000,
                                    height_panel = 2,
                                    energy = np.linspace(5,20,12),
                                    subsidy=0.0,
-                                   lifetime = 30):
+                                   lifetime = 30)
     
 
     monthly_df = pd.concat([df_energyOut, df_energyUse, df_agricultural], axis=1)
@@ -29,7 +57,7 @@ def combine_dataframes(crop_type = "potatoes"):
     return monthly_df, single_df
 
 def main():
-    montly_df, single_df = combine_dataframes()
+    montly_df, single_df = interface()
     df_name = os.path.join(dir_path, "output", f"total_df.csv")
     montly_df.to_csv(df_name)
     print(montly_df)
