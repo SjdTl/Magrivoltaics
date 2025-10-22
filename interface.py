7dc8d67c1bf669f3ae2cde96e2e5376ff1dd0622
SjdTl <terlouwsjoerd@outlook.com>
2025-10-22 14:59:19 +0200

    changed interface

--- interface.py
+++ interface.py
@@ -7,18 +7,23 @@ import os as os
 import numpy as np
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-def combine_dataframes():
+def combine_dataframes(crop_type = "potatoes"):
     df_energyOut = energy_output()
     df_energyUse = energy_usage()
-    df_energyExport = df_energyOut["Energy output [kWh]"] - df_energyUse["Energy usage [kWh]"]
-    df_energyExport.name = "Energy export [kWh]"
-    df_agricultural = agricultural(energy_output = np.array(df_energyOut["Energy output [kWh]"]),
-                                   irradiation= np.array(df_energyOut["Irradiation [kWh/m^2]"]))
+    df_energyOut["Energy export [kWh]"] = df_energyOut["Energy output [kWh]"] - df_energyUse["Energy usage [kWh]"]
 
-    df_economicsSingle, df_economisMontly = economics(energy = np.array(df_energyExport))
+    df_agricultural = agricultural(crop_type = crop_type, irradiation_crop= np.array(df_energyOut["Irradiation crops [kWh/m^2]"]))
+    df_economicsSingle = economics(area=10000,
+                                   coverage =0.5,
+                                   rows =10,
+                                   length_rows =1000,
+                                   height_panel = 2,
+                                   energy = np.linspace(5,20,12),
+                                   subsidy=0.0,
+                                   lifetime = 30):
     
 
-    monthly_df = pd.concat([df_energyOut, df_energyUse, df_economisMontly, df_energyExport, df_agricultural],axis=1)
+    monthly_df = pd.concat([df_energyOut, df_energyUse, df_agricultural], axis=1)
     single_df = pd.concat([df_economicsSingle], axis=1)
 
     return monthly_df, single_df
