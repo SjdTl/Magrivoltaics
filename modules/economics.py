e93b86f198d405ca2a61de701fc157ba13e4f7d7
arepriels <125666348+arepriels@users.noreply.github.com>
2025-10-22 14:30:48 +0200

    Update economics.py
    finished

--- modules/economics.py
+++ modules/economics.py
@@ -6,8 +6,13 @@ months = [
         "July", "August", "September", "October", "November", "December"
     ]
 
-def economics(subsidy : float = 10,
-              energy = np.linspace(5,20,12),
+def economics(area=10000,                           # total farm area [m²]
+              coverage: float =0.5,                       # fraction of area covered by PV (0–1)
+              rows: float =10,                           # number of rows in the farm
+              length_rows: float =1000,                    # length of each row [m]
+              height_panel: float = 2,                   # height panel
+              energy = np.linspace(5,20,12),  # pandas DataFrame: monthly energy production [kWh/month]
+              subsidy=0.0,                    # subsidy fraction of CAPEX (0–1)
               lifetime : float = 30):
     """
     Description
@@ -41,21 +46,52 @@ def economics(subsidy : float = 10,
     --------
 
     """
-    total_cost = 1e6
-    LCOE = (total_cost - subsidy) / (lifetime * np.sum(energy))
+
+    # area calculations
+    panel_area_m2 = 2.58
+    area_pv = area * coverage
+    n_panels = area_pv / panel_area_m2
+    p_sys_kW = (n_panels * 580) / 1000  # total system capacity [kW]
+
+    # estimated CAPEX costs 
+    panel_costs=499*n_panels*0.8          # installed cost [€]
+    #mounting_costs= 19.22*rows*length_rows + 72.89*rows*n_panels*height_panel
+    mounting_costs=panel_costs/250
+    installation_costs=100*p_sys_kW 
+    BOP_costs=1048.5*p_sys_kW 
+    CAPEX = panel_costs + mounting_costs + installation_costs + BOP_costs      # [€]
+
+    OM = 35 * p_sys_kW #[€]
+
+    #capital recovery factor
+    r=0.0215
+    alpha =  (r * (1 + r) ** lifetime) / ((1 + r) ** lifetime - 1)
+    #LCOE 
+    annual_energy_kWh = np.sum(energy)   # sum of 12 months
+    LCOE= ( (alpha * (CAPEX - subsidy))+ OM ) / annual_energy_kWh 
+    #ROI
+    energy_price = 0.1301 #€/kWh
+    net_profit= (energy_price-LCOE)*annual_energy_kWh
+    ROI=(net_profit/CAPEX)*100
+
     single_parameters = pd.DataFrame({
         "LCOE" : [LCOE],
-         "ROI" : [4],
+         "ROI" : [ROI],
          })
 
-    maintenance_cost = 12*[10]
-    energy_price = 12*[10]
-    monthly_parameters = pd.DataFrame(np.transpose([maintenance_cost, energy_price]), 
-                                      columns=["Maintenance cost [eur]", "Energy price [eur]"], 
-                                      index=months)
-    return single_parameters, monthly_parameters
+    maintenance_cost = OM
 
+    return maintenance_cost, energy_price, LCOE, ROI
+
+# ===== Test the function =====
 if __name__ == '__main__':
-    s, m = economics()
+    s, m, q, r = economics(
+    )
+    print("Operation & maintenance costs [€/year]")
     print(s)
+    print("\nEnergy price [€/kWh]")
+    print(m)
+    print("\nLCOE [€/kWh]")
+    print(q)
+    print("\nROI")
     print(m)
